0c0c0918fc0597e3166bcd19cac2436c8e7224fe
Aaron Bell <abell@io.com>
2015-01-16 18:59:41 -0800

    GH-24 adding kwargs

--- _modules/consul_mod.py
+++ _modules/consul_mod.py
@@ -2,11 +2,6 @@
 '''
 Execution module to provide consul functionality to Salt
 
-:maintainer: Aaron Bell <aarontbellgmail.com>
-:maturity: new
-:depends:    - python-consul (http://python-consul.readthedocs.org/en/latest/)
-:platform: Linux
-
 .. versionadded:: 2014.7.0
 
 :configuration: This module requires the python-consul python module and uses the
@@ -41,7 +36,7 @@ def __virtual__():
         return False
 
 
-def _connect(host='localhost', port=8500, consistency='default'):
+def _connect(host='localhost', port=8500, consistency='default', **kwargs):
     '''
     Returns an instance of the consul client
     '''
@@ -54,7 +49,7 @@ def _connect(host='localhost', port=8500, consistency='default'):
     return consul.Consul(host, port, consistency)
 
 
-def key_delete(key, recurse=None):
+def key_delete(key, recurse=None, **kwargs):
     '''
     Deletes the keys from consul, returns number of keys deleted
 
@@ -64,9 +59,7 @@ def key_delete(key, recurse=None):
 
         salt '*' consul.key_delete foo
     '''
-    # Get connection args from keywords if set
-
-    c = _connect()
+    c = _connect(**kwargs)
     index, data = c.kv.get(key)
     if not data:
         return False
@@ -74,7 +67,7 @@ def key_delete(key, recurse=None):
         return c.kv.delete(key, recurse)
 
 
-def key_exists(key):
+def key_exists(key, **kwargs):
     '''
     Return true if the key exists in consul
 
@@ -84,7 +77,7 @@ def key_exists(key):
 
         salt '*' consul.key_exists foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     index, data = c.kv.get(key)
     if not data:
         return False
@@ -92,7 +85,7 @@ def key_exists(key):
         return True
         
 
-def key_get(key):
+def key_get(key, **kwargs):
     '''
     Gets the value of the key in consul
 
@@ -102,7 +95,7 @@ def key_get(key):
 
         salt '*' consul.key_get foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     index, data = c.kv.get(key)
     if not data:
         return False
@@ -110,7 +103,7 @@ def key_get(key):
         return data['Value']
 
 
-def key_put(key, value):
+def key_put(key, value, **kwargs):
     '''
     Sets the value of a key in consul
 
@@ -120,20 +113,20 @@ def key_put(key, value):
 
         salt '*' consul.key_put foo bar
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     c.kv.put(key, value)
     index, data = c.kv.get(key)
     return data['Value']
 
 
-def service_list(catalog=False, dc=None, index=None):
+def service_list(catalog=False, dc=None, index=None, **kwargs):
     '''
     List services known to Consul
     CLI Example:
     .. code-block:: bash
         salt '*' consul.service_list
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     services = []
     if catalog:
         index, services = c.catalog.services(dc, index)
@@ -143,7 +136,7 @@ def service_list(catalog=False, dc=None, index=None):
     return services
 
 
-def service_get(name, dc=None, tag=None, index=None):
+def service_get(name, dc=None, tag=None, index=None, **kwargs):
     '''
     Get a Consul service's details
 
@@ -153,14 +146,14 @@ def service_get(name, dc=None, tag=None, index=None):
 
         salt '*' consul.service_get
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     for service, data in c.agent.services().items():
         if name == service:
             return data
     return False
 
 
-def service_register(name, service_id=None, port=None, tags=None, script=None, interval=None, ttl=None):
+def service_register(name, service_id=None, port=None, tags=None, script=None, interval=None, ttl=None, **kwargs):
     '''
     Register a service with Consul
 
@@ -170,11 +163,11 @@ def service_register(name, service_id=None, port=None, tags=None, script=None, i
 
         salt '*' consul.service_register foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     return c.agent.service.register(name, service_id, port, tags, script, interval, ttl)
 
 
-def service_deregister(name):
+def service_deregister(name, **kwargs):
     '''
     Deregister a service from Consul
 
@@ -184,13 +177,13 @@ def service_deregister(name):
 
         salt '*' consul.service_deregister foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     if name not in service_list():
         return False
     return c.agent.service.deregister(name)
 
 
-def check_list():
+def check_list(**kwargs):
     '''
     List checks known to Consul
 
@@ -200,14 +193,14 @@ def check_list():
 
         salt '*' consul.check_list
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     checks = []
     for check, data in c.agent.checks().items():
         checks.append(check)
     return checks
 
 
-def check_get(name):
+def check_get(name, **kwargs):
     '''
     Get the details of a check in Consul
 
@@ -217,14 +210,14 @@ def check_get(name):
 
         salt '*' consul.check_get
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     for check, data in c.agent.checks().items():
         if name == check:
             return data
     return False
 
 
-def check_register(name, check_id=None, script=None, interval=None, ttl=None, notes=None):
+def check_register(name, check_id=None, script=None, interval=None, ttl=None, notes=None, **kwargs):
     '''
     Register a check with Consul
 
@@ -234,11 +227,11 @@ def check_register(name, check_id=None, script=None, interval=None, ttl=None, no
 
         salt '*' consul.check_register foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     return c.agent.check.register(name, check_id, script, interval, ttl, notes)
 
 
-def check_deregister(name):
+def check_deregister(name, **kwargs):
     '''
     Deregister a check from Consul
 
@@ -248,13 +241,13 @@ def check_deregister(name):
 
         salt '*' consul.check_deregister foo
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     if name not in check_list():
         return False
     return c.agent.check.deregister(name)
 
 
-def get_service_status(name,index=None, passing=None):
+def get_service_status(name,index=None, passing=None, **kwargs):
     '''
     Get the health status of a service in Consul
 
@@ -264,7 +257,7 @@ def get_service_status(name,index=None, passing=None):
 
         salt '*' consul.get_service_status
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     index, nodes = c.health.service(name, index, passing)
     node_list = []
     for node in nodes:
@@ -274,7 +267,7 @@ def get_service_status(name,index=None, passing=None):
     return node_list
 
 
-def node_list():
+def node_list(**kwargs):
     '''
     List nodes in Consul
 
@@ -284,7 +277,7 @@ def node_list():
 
         salt '*' consul.node_list
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     node_list = []
     index, nodes = c.catalog.nodes()
     for node in nodes:    
@@ -292,7 +285,7 @@ def node_list():
     return node_list
 
 
-def node_get(name, dc=None, tag=None, index=None):
+def node_get(name, dc=None, tag=None, index=None, **kwargs):
     '''
     Get a Consul node's details
 
@@ -302,14 +295,14 @@ def node_get(name, dc=None, tag=None, index=None):
 
         salt '*' consul.node_get
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     index, node = c.catalog.node(name, dc, tag, index)
     if not node:
         return False
     return node
 
 
-def dc_list():
+def dc_list(**kwargs):
     '''
     List datacenters in Consul
 
@@ -319,11 +312,11 @@ def dc_list():
 
         salt '*' consul.dc_list
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     return c.catalog.datacenters()
 
 
-def ttl_pass(name, notes=None, type='check'):
+def ttl_pass(name, notes=None, type='check', **kwargs):
     '''
     Mark a ttl-based service or check as passing
 
@@ -335,13 +328,13 @@ def ttl_pass(name, notes=None, type='check'):
         
         salt '*' consul.ttl_pass foo type=service
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     if type == 'service':
         name = 'service:' + name
     return c.agent.check.ttl_pass(name, notes)
 
 
-def ttl_warn(name, notes=None, type='check'):
+def ttl_warn(name, notes=None, type='check', **kwargs):
     '''
     Mark a ttl-based service or check as warning
 
@@ -353,13 +346,13 @@ def ttl_warn(name, notes=None, type='check'):
         
         salt '*' consul.ttl_warn foo type=service
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     if type == 'service':
         name = 'service:' + name
     return c.agent.check.ttl_warn(name, notes)
 
 
-def ttl_fail(name, notes=None, type='check'):
+def ttl_fail(name, notes=None, type='check', **kwargs):
     '''
     Mark a ttl-based service or check as failing
 
@@ -371,7 +364,7 @@ def ttl_fail(name, notes=None, type='check'):
         
         salt '*' consul.ttl_fail foo type=service
     '''
-    c = _connect()
+    c = _connect(**kwargs)
     if type == 'service':
         name = 'service:' + name
     return c.agent.check.ttl_fail(name, notes)
